78eb8c2637bbca646f85c5d0f0d9bf82752ca970
lisi <lisi@qq.com>
2022-03-05 17:58:14 +0800

    master first commit

---
+++ src/GitTest01.java
@@ -0,0 +1,13 @@
+/**
+ * @author mintFM
+ * @create 2022-03-05 17:56
+ */
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("master git1");
+        System.out.println("master git1");
+        System.out.println("master git1");
+        System.out.println("master git1");
+
+    }
+}
